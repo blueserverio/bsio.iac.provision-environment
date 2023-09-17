e7b984f59f58e6fdaab40ecdbd0ada85c7f295fb
Scott Blue <scotch16@gmail.com>
2023-09-16 23:23:55 -0500

    fixes #7. Added the chocolatey script to install git.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,6 +4,9 @@
 # Install Chololatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Install Git
+choco install git -y
+
 # Create the Ansible User
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/blueserverio/bsio.iac.provision-environment/main/CreateAnsibleUser.ps1'))
 
