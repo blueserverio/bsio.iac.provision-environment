a864fd7020cf642316cdfb49eee9b3ddcdaec459
Scott Blue <scotch16@gmail.com>
2023-09-17 00:08:00 -0500

    fixes #19. Added a check to see if the user already exists.

--- CreateAnsibleUser.ps1
+++ CreateAnsibleUser.ps1
@@ -17,18 +17,20 @@ if (-Not $myWindowsPrincipal.IsInRole($adminRole))
 }
 
 # Create new local Admin user for script purposes
+$ansibleUser = Get-LocalUser -Name "ansible" -ErrorAction SilentlyContinue
+if ($null -eq $ansibleUser){
+    $Computer = [ADSI]"WinNT://$Env:COMPUTERNAME,Computer"
 
-$Computer = [ADSI]"WinNT://$Env:COMPUTERNAME,Computer"
+    $LocalAdmin = $Computer.Create("User", "ansible")
+    $LocalAdmin.SetPassword("Password1!")
+    $LocalAdmin.SetInfo()
+    $LocalAdmin.FullName = "Local Ansible account by Powershell"
+    $LocalAdmin.SetInfo()
+    $LocalAdmin.UserFlags = 64 + 65536 # ADS_UF_PASSWD_CANT_CHANGE + ADS_UF_DONT_EXPIRE_PASSWD
+    $LocalAdmin.SetInfo()
 
-$LocalAdmin = $Computer.Create("User", "ansible")
-$LocalAdmin.SetPassword("Password1!")
-$LocalAdmin.SetInfo()
-$LocalAdmin.FullName = "Local Ansible account by Powershell"
-$LocalAdmin.SetInfo()
-$LocalAdmin.UserFlags = 64 + 65536 # ADS_UF_PASSWD_CANT_CHANGE + ADS_UF_DONT_EXPIRE_PASSWD
-$LocalAdmin.SetInfo()
+    $group = [ADSI]("WinNT://"+$env:COMPUTERNAME+"/administrators,group")
+    $group.add("WinNT://ansible,user")
 
-$group = [ADSI]("WinNT://"+$env:COMPUTERNAME+"/administrators,group")
-$group.add("WinNT://ansible,user")
-
-Write-Host "PS Remoting User ('Ansible') has been successfully created."
+    Write-Host "PS Remoting User ('Ansible') has been successfully created."
+}
